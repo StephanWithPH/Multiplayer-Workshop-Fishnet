90367c9d9f80ae56d273faadf5dc65f1a1d56c91
Aron Maijen <aron@mayen.at>
2024-03-20 10:37:23 +0100

    Added BulletLogic

--- FishnetWorkshop/Assets/Scripts/GunLogic.cs
+++ FishnetWorkshop/Assets/Scripts/GunLogic.cs
@@ -1,10 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
+using FishNet.Connection;
+using FishNet.Object;
 using UnityEngine;
 
-public class GunLogic : MonoBehaviour
+public class GunLogic : NetworkBehaviour
 {
+    [SerializeField] GameObject bulletPrefab;
+    [SerializeField] Transform shootPoint;
+    
     void OnFire() {
+        Shoot();
+    }
+    
+    [ServerRpc(RequireOwnership = false)]
+    void Shoot(NetworkConnection client = null) {
         print("Shoot her!");
+        GameObject bull = Instantiate(bulletPrefab, shootPoint.position, shootPoint.rotation);
+        Spawn(bull, client);
     }
 }
